737085feaa92c94a0923e66f9215a450a05d9d8b
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2014-08-27 00:49:53 -0300

    Use the Client class in client.c file
    This simplifies a lot the client code.

--- client.c
+++ client.c
@@ -12,9 +12,6 @@
 #include "client.h"
 #include "screen.h"
 
-// hold the nickname by parameter
-char *nickname;
-
 void helpMessage()
 {
 	std::cout << "You need to give the address number and nickname as argument!";
@@ -46,64 +43,29 @@ int main(int argc, char *argv[])
 	if (argc < 3)
 		helpMessage();
 
-	struct sockaddr_in server;
-	int sock_server;
+	Client client(argv[2]);
 
-	sock_server = socket(PF_INET, SOCK_STREAM, 0);
-
-	bzero(&server, sizeof(server));
-
-	server.sin_family = PF_INET;
-	server.sin_port = htons(CHAT_PORT);
-	inet_aton(argv[1], &server.sin_addr);
-
-	int ret = connect(sock_server, (struct sockaddr *)&server, (socklen_t)sizeof(server));
-
-	if (ret)
+	if (!client.server_connect(argv[1])) {
 		perror("Can't connect to server");
-	else
-	{
-		init_screen();
-
-		struct chat_message cm;
-		cm.type = REGISTER;
-		strncpy(cm.nickname, argv[2], sizeof(cm.nickname));
-
-		send(sock_server, &cm, sizeof(cm), 0);
-
-		char ret[100];
-		recv(sock_server, &ret, sizeof(ret), 0);
-
-		if (!strcmp(ret, CHAT_OK))
-		{
-			add_message("Received OK from server...");
-
-			pthread_t thread_id;
-			pthread_create(&thread_id, NULL, &receive_message_from_users, &sock_server);
-			cm.type = SEND_MESSAGE;
-
-			while (1)
-			{
-				//fgets(cm.msg, sizeof(cm.msg), stdin);
-				std::vector<char> msg(100);
-				get_user_input(msg);
-
-				std::string nmsg(msg.begin(), msg.end());
-
-				strcpy(cm.msg, nmsg.c_str());
-				cm.msg[strlen(cm.msg)] = '\0';
-
-				if (send(sock_server, &cm, sizeof(cm), 0) == -1) {
-					std::cout << "Error while sending message to server. Aborting...\n";
-					break;
-				}
-			}
-		}
-
-		end_screen();
+		return 1;
 	}
 
-	close(sock_server);
+	init_screen();
+
+	if (client.send_register_message())
+	{
+		add_message("Received OK from server...");
+
+		pthread_t thread_id;
+		int server_sock = client.get_server_sock();
+		pthread_create(&thread_id, NULL, &receive_message_from_users, &server_sock);
+
+		while (1)
+			if (!client.send_user_message())
+				break;
+	}
+
+	end_screen();
 
 	return 0;
 }
