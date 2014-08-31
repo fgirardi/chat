079545df8a643bef93dfedab6c894d9ec2445d6b
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2014-08-31 18:03:19 -0300

    Added some more TODO notes

--- server.c
+++ server.c
@@ -14,6 +14,9 @@
 /*
 TODO:
 * Build an keep alive to close the socket from dead clients
+* Create server class
+* Convert chat_message into a class
+* Convert threads array into std::vector
 */
 
 /* file descriptors of sockets */
