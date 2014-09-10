8df86bdd7bfe641c3069d9cf127ed5cabd85e9c0
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2014-09-09 23:53:13 -0300

    Remove useless includes of server.c

--- server.c
+++ server.c
@@ -1,15 +1,3 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <iostream>
-#include <pthread.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <time.h>
-#include <unistd.h>
-
-#include "chat.h"
 #include "server.h"
 
 int main()
